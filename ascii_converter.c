4aa85bef452d8d16343715094d57da73fb192a97
mhonda2 <60487127+mhonda2@users.noreply.github.com>
2020-03-15 15:54:19 -0700

    Update ascii_converter.c

--- ascii_converter.c
+++ ascii_converter.c
@@ -1,9 +1,11 @@
 //Converts ASCII code to character
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <stdbool.h>
+#include "openfiles.h"
 
+#define LEN 15
 
 int convert(const char* string);
 
@@ -14,40 +16,71 @@ int main(int argc, const char* argv[])
               "SI","DLE","DC1","DC2","DC3","DC4","NAK","SYN","ETB",
             "CAN","EM","SUB","ESC","FS","GS","RS","US"};
   const char* parity[] = {"Even","Odd"};
-  //
-  // FILE* fin;
-  // FILE* fout;
-  // int n;
 
-  char* asc = "01000001";
-  printf("%s is %d\n", asc, convert(asc));
-  // bool success = openfile(argc, argv, &fin, &fout, &n);
-  // if (!success) { exit(1); }
-  //
-  // char* s[n];
-  // int par;
-  // int code;
-  //
-  // while (fgets(s[], n, fin) != EOF)
-  // {
-  //   code = convert(s, n);
-  //
-  //   /*printf("Original\tASCII\tDecimal\t");*/
-  //   /*printf("%s\t%c\t%\t%d\t%s",);*/
-  //
-  //   printf("%d\n", code);
-  // }
-return 0;
+  FILE* fin;
+  FILE* fout;
+  int n;
+
+   bool success = openfiles(argc, argv, &fin, &fout, &n);
+   if (!success) { exit(1); }
+
+   char s[LEN];
+   int par;
+   int code;
+   n = 10;
+
+   //Header
+   printf("Original\tASCII\tDecimal\tParity\n");
+   fputs("Original\tASCII\tDecimal\tParity\n", fout);
+
+   while (fgets(s, n, fin) != NULL)
+   {
+     if(strlen(s) > 8) s[8] = '\0';
+     else continue;
+
+     code = convert(s);
+     par = (code % 2 == 0) ? 0 : 1;
+
+     //Binary
+     printf("%s\t", s);
+     fputs(s, fout);
+     fputs("\t", fout);
+
+     //Ascii character
+     if(code <= 31) {
+       printf("%s\t", names[code]);
+       fputs(names[code], fout);
+     }
+     else {
+       printf("%c\t", code);
+       fputc(code, fout);
+     }
+     fputs("\t", fout);
+
+     //Ascii number
+     printf("%d\t", code);
+     fputc('0', fout);  //Need to convert number to char
+     fputs("\t", fout);
+
+     //parity
+     printf("%s\n", parity[par]);
+     fputs(parity[par], fout);
+     fputc('\n', fout);
+
+   }
+   closefiles(fin, fout);
+   return 0;
 }
 
 int convert(const char* string)
 {
   int decimal = 0;
-  int size = strlen(string) - 2;  // skipping extended ascii
-  while (size-- >= 0) {
-    int i = (string[size] - '0') * pow(2, size);
+  int j = 0;
+  int size = strlen(string);
+  while (size-- > 0) {
+    int i = (string[size] - '0') * pow(2, j);
     decimal += i;
+    ++j;
   }
   return decimal;
 }
-
